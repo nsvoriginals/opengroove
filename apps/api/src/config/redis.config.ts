d15e97032620175e7428b92ad9819b08014c63c8
sheshi vardhan <123223662+nsvoriginals@users.noreply.github.com>
2026-01-16 10:20:07 +0530

    Update Redis client URL in redis.config.ts

--- apps/api/src/config/redis.config.ts
+++ apps/api/src/config/redis.config.ts
@@ -1,7 +1,7 @@
 import { createClient, RedisClientType } from 'redis';
 
 export const rClient:RedisClientType = createClient({
-  url: "rediss://default:ATzdAAIncDE3NTRlN2JlMjc5YzE0MDNhYmQ3MmI0MDkwN2M1OWFiN3AxMTU1ODE@cuddly-wren-15581.upstash.io:6379",
+  url: "FUCK OFF ",
   socket: {
     reconnectStrategy: (retries) => {
       if (retries > 10) {
